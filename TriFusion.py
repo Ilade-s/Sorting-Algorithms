327111b903b45868791fa75f5220349028183258
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-04-03 17:03:11 +0200

    TriFusion 4 elements

--- TriFusion.py
+++ TriFusion.py
@@ -13,14 +13,67 @@ def TriFusion(l, reverse=False):
 
     PARAMETRES :
     ------------
-    - it : iterable : list || str
-        - iterable that needs to be sorted
+    - l : list
+        - list that needs to be sorted
     - reverse : bool
         - If False, sort in asending order
         - If True, sort in descending order
             - default = False
     
     RETURNS :
-    - it : iterable (same type as it)
-        sorted iterable
+    - l : list (same type as l)
+        sorted list
     """
+    n = len(l)
+
+    if n<=1: 
+        return l
+
+    else:
+        A = l[:n//2]
+        B = l[n//2:]
+        print(A)
+        print(B)
+        # tri sur place
+        if len(A)<=1:
+            pass
+        elif A[0] <= A[1]:
+            A = [A[0],A[1]]
+        else:
+            A = [A[1],A[0]]
+        if len(B)<=1:
+            pass
+        elif B[0] <= B[1]:
+            B = [B[0],B[1]]
+        else:
+            B = [B[1],B[0]]
+
+        return(fusion(A,B,n))
+
+def fusion(A,B,n):
+
+    f = []
+    while len(f)<n:
+        if A==[] or B==[]:
+            if A==[]: 
+                f.append(B.pop(0))
+            elif B==[]: 
+                f.append(A.pop(0))
+        else:
+            if A[0] <= B[0]:
+                f.append(A.pop(0))
+            else:
+                f.append(B.pop(0))
+    
+    return f
+
+if __name__=='__main__': # Test
+    import random as rnd
+    r = 8
+    l = [4,1,3,2]
+    l = [rnd.randint(0,r) for i in range(r)]
+    print(l)
+    l = TriFusion(l)
+    print("liste triée :",l)
+
+
