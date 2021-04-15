27bfd7d330bf18beb0d9c39cd232da02a4bbcbf9
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-04-15 20:40:57 +0200

    Fix TriFusion.py

--- TriFusion.py
+++ TriFusion.py
@@ -2,11 +2,7 @@
 Second sorting algorithm
 Principle : https://fr.wikipedia.org/wiki/Tri_fusion
 """
-import sys
-sys.setrecursionlimit(10**6)
 from time import perf_counter_ns
-from copy import deepcopy
-
 
 def TriFusion(l):
     """
@@ -16,63 +12,34 @@ def TriFusion(l):
 
     if n<=1: # Si vide ou un seul éléments, alors déjà trié
         return l
-
-    elif n>1000: # Pour eviter un stack overflow / Recursion error (ne corrige rien du tout... casse encore plus vite)
-        listtris = []
-        # parties complètes de len = 400     
-        #print("parts entières :",n//400)
-        for p in range(n//400):
-            i = p*400
-            il = l[i:i+400]
-            ilen = len(il)
-            listtris.append(fusion(TriFusion(il[:ilen//2]),TriFusion(il[ilen//2:])))
-        if n%400!=0:
-            # partie finale/incomplète (si existante)
-            #print("élements restants :",n-(n//400)*400)
-            fl = l[(n//400)*400:]
-            flen = len(fl)
-            listtris.append(fusion(TriFusion(fl[:flen//2]),TriFusion(fl[flen//2:])))
-        #print(listtris)
-        while len(listtris)>1:
-            #print(len(listtris))
-            tmp = []
-            for i in range(0,len(listtris),2):
-                try:
-                    tmp.append(fusion(listtris[i],listtris[i+1]))
-                except:
-                    tmp.append(listtris[i])
-            listtris = deepcopy(tmp)
-            
-
         
-        #print(listtris)
-        #print(len(listtris))
-        return listtris[0]
-
-
-        
-
     else: 
-        return fusion(TriFusion(l[:n//2]),TriFusion(l[n//2:]))
+        sl = []
+        A = l[:n//2]
+        B = l[n//2:]
 
+        A = TriFusion(A)
+        B = TriFusion(B)
 
-def fusion(A,B):
-    """
-    fusionne A et B
-    """
-    if A==[]:
-        return B
-    elif B==[]:
-        return A
-    
-    elif A[0]<=B[0]:
-        return [A[0]] + fusion(A[1:], B)
-    
-    elif B[0]<=A[0]:
-        return [B[0]] + fusion(A, B[1:])
+        while len(sl)<n:
+            while A!=[] or B!=[]:
+                if A==[]:
+                    sl.append(B.pop(0))
+
+                elif B==[]:
+                    sl.append(A.pop(0))
+                
+                elif A[0]<=B[0]:
+                    sl.append(A.pop(0))
+                
+                elif B[0]<=A[0]:
+                    sl.append(B.pop(0))
+             
+        return sl
 
 if __name__=='__main__': # Test
     import random as rnd
+    #rlist = [5,500,2000,5000,10000,15000,20000,1000000]
     r = int(input("Nombre d'éléments dans la liste : "))
     #r = 20000
     #l = [4,1,3,2]
@@ -84,14 +51,14 @@ if __name__=='__main__': # Test
     end = perf_counter_ns()
     execution_time = round((end - start)*10**(-6),3)
     print("Time passed (ms) :",execution_time)
-    #timeListFS.append(execution_time)
-    #print("liste triée :",sl)
-    #print(len(sl))
-    print("Tri fait")
     if sl == sorted(l):
         print("Tri correct")
     else : 
         print("Tri incorrect")
+    #timeListFS.append(execution_time)
+    #print("liste triée :",sl)
+    #print(len(sl))
+    #print("Tri fait")
     #print(timeListFS)
 
 
